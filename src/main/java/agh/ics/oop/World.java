305bf616b4742e3ed77f8471bd9e32dd93f5da80
klobt <111685189+klobt@users.noreply.github.com>
2022-10-17 13:00:21 +0200

    Lab1: Punkt 7.
    Dodaj metodę statyczną run, która jest wywoływana pomiędzy tymi komunikatami.

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,6 +3,8 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] arguments) {
         System.out.println("system wystartował");
+        run();
         System.out.println("system zakończył działanie");
     }
+    private static void run() {}
 }
