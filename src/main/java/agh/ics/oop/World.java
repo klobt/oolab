367547792f250e72fbc690931e79f03ead000450
klobt <111685189+klobt@users.noreply.github.com>
2022-10-17 13:01:42 +0200

    Lab1: Punkt 10.
    Rozszerz metodę run tak by akceptowała tablicę argumentów typu String.

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,10 +3,10 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] arguments) {
         System.out.println("system wystartował");
-        run();
+        run(arguments);
         System.out.println("system zakończył działanie");
     }
-    private static void run() {
+    private static void run(String[] arguments) {
         System.out.println("zwierzak idzie do przodu");
     }
 }
