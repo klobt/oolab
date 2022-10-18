ca34ec05b5f7b0fb3d5071a88ee3f4a9b3c8b694
klobt <111685189+klobt@users.noreply.github.com>
2022-10-18 19:29:12 +0200

    Lab2: MapDirection: implementacja

---
+++ src/main/java/agh/ics/oop/MapDirection.java
@@ -0,0 +1,42 @@
+package agh.ics.oop;
+
+public enum MapDirection {
+    NORTH, SOUTH, WEST, EAST;
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case NORTH -> "Północ";
+            case SOUTH -> "Południe";
+            case WEST -> "Zachód";
+            case EAST -> "Wschód";
+        };
+    }
+
+    public MapDirection next() {
+        return switch (this) {
+            case NORTH -> MapDirection.EAST;
+            case EAST -> MapDirection.SOUTH;
+            case SOUTH -> MapDirection.WEST;
+            case WEST -> MapDirection.NORTH;
+        };
+    }
+
+    public MapDirection previous() {
+        return switch (this) {
+            case NORTH -> MapDirection.WEST;
+            case EAST -> MapDirection.NORTH;
+            case SOUTH -> MapDirection.EAST;
+            case WEST -> MapDirection.SOUTH;
+        };
+    }
+
+    public Vector2d toUnitVector() {
+        return switch (this) {
+            case NORTH -> new Vector2d(0, 1);
+            case EAST -> new Vector2d(1, 0);
+            case SOUTH -> new Vector2d(0, -1);
+            case WEST -> new Vector2d(-1, 0);
+        };
+    }
+}
