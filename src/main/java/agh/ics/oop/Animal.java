af4c972b315b66cbe5ed1786bd088d6e6c1b8efc
klobt <111685189+klobt@users.noreply.github.com>
2022-11-14 15:00:44 +0100

    Revert "Lab4: Podpunkt 3."
    This reverts commit ed2e599e21ee23e0d9392af3d4111e6d0e8d336c.

--- src/main/java/agh/ics/oop/Animal.java
+++ src/main/java/agh/ics/oop/Animal.java
@@ -3,16 +3,10 @@ package agh.ics.oop;
 public class Animal {
     private Vector2d position = new Vector2d(2, 2);
     private MapDirection orientation = MapDirection.NORTH;
-    private IWorldMap map;
 
     @Override
     public String toString() {
-        return switch (orientation) {
-            case NORTH -> "^";
-            case EAST -> ">";
-            case SOUTH -> "v";
-            case WEST -> "<";
-        };
+        return position.toString() + " " + orientation.toString();
     }
 
     public boolean isAt(Vector2d position) {
@@ -28,12 +22,7 @@ public class Animal {
     }
 
     private void setPosition(Vector2d position) {
-        if (map != null) {
-            if (map.canMoveTo(position)) {
-                this.position = position;
-                map.place(this);
-            }
-        } else if (position.x >= 0 && position.x <= 4 && position.y >= 0 && position.y <= 4) {
+        if (position.x >= 0 && position.x <= 4 && position.y >= 0 && position.y <= 4) {
             this.position = position;
         }
     }
@@ -45,18 +34,5 @@ public class Animal {
             case FORWARD -> setPosition(position.add(orientation.toUnitVector()));
             case BACKWARD -> setPosition(position.subtract(orientation.toUnitVector()));
         }
-    }
-
-    public Animal(IWorldMap map) {
-        this.map = map;
-    }
-
-    /* Można ustawić wartości domyślne dla argumentów konstruktora
-       `null` dla `map` i `new Vector2d(2, 2)` dla `initialPosition`.
-       W ten sposób pozostałe konstruktory nie będą konieczne.
-     */
-    public Animal(IWorldMap map, Vector2d initialPosition) {
-        this.map = map;
-        setPosition(initialPosition);
     }
 }
