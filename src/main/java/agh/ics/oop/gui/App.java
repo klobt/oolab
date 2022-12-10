06b127dadad26eddbbe578efad06d9a8372ff44f
klobt <111685189+klobt@users.noreply.github.com>
2022-12-10 22:42:57 +0100

    Lab7: Wyświetlanie mapy w GUI: Punkt 3. i 4.

--- src/main/java/agh/ics/oop/gui/App.java
+++ src/main/java/agh/ics/oop/gui/App.java
@@ -1,5 +1,6 @@
 package agh.ics.oop.gui;
 
+import agh.ics.oop.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -7,6 +8,13 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    MoveDirection[] directions;
+    IWorldMap map;
+    @Override
+    public void init() {
+        directions = new OptionsParser().parse(getParameters().getRaw().toArray(new String[0]));
+        map = new RectangularMap(10, 5);
+    }
     @Override
     public void start(Stage primaryStage) {
         GridPane gridPane = new GridPane();
