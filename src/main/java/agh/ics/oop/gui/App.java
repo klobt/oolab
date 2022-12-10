f07d37c848b982da13cc87014c74865e8e10f8e6
klobt <111685189+klobt@users.noreply.github.com>
2022-12-10 22:33:21 +0100

    Lab7: Wyświetlanie mapy w GUI: Punkt 2.

--- src/main/java/agh/ics/oop/gui/App.java
+++ src/main/java/agh/ics/oop/gui/App.java
@@ -3,13 +3,22 @@ package agh.ics.oop.gui;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Zwierzak");
-        Scene scene = new Scene(label, 400, 400);
+        GridPane gridPane = new GridPane();
+
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 4; x++) {
+                Label label = new Label("Zwierzak");
+                gridPane.add(label, x, y);
+            }
+        }
+
+        Scene scene = new Scene(gridPane, 400, 400);
 
         primaryStage.setScene(scene);
         primaryStage.show();
